e95b29880e4cf16c64b871cfdd40bddcf7050d7b
Sergiio086 <sjimenezg@elpuig.xeill.net>
2022-11-07 16:00:19 +0100

    Practica

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fileReader = new FileReader("src/santako.txt");
+
+    }
+}
